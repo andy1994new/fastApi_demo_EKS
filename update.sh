bb865b8d5751682980bb7a957a7741d58b7ef170
Andy G <andyg@Andys-MacBook-Air.local>
2025-01-29 18:57:33 +0000

    +4

--- update.sh
+++ update.sh
@@ -37,6 +37,12 @@ cat $tmp_dir/$yaml_name
 # Update image tag in deployment YAML
 sed -i -e "s|andy2025/$image_name:.*|andy2025/$image_name:$new_ver|g" $tmp_dir/$yaml_name
 
+echo "After sed:"
+cat $tmp_dir/$yaml_name
+
+echo "After sed, git status:"
+git status
+
 # Navigate to repo
 cd $tmp_dir
 
